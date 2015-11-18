8b72c2db9749f3701368763c478e55f43de59991
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-11-17 17:08:31 -0800

    Initial commit with tabs

---
+++ www/js/controllers.js
@@ -0,0 +1,20 @@
+angular.module('starter.controllers', [])
+
+
+
+  .controller('CreateNewCtrl', function($scope, $stateParams) {
+
+  })
+
+  .controller('PastCtrl', function($scope, $stateParams) {
+
+  })
+
+  .controller('ContactsCtrl', function($scope, $stateParams) {
+
+  })
+
+  .controller('SettingsCtrl', function($scope, $stateParams) {
+
+  });
+
